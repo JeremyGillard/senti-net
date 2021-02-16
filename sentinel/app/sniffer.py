5e7eb9ba5e9f6009d88acfe34eda7e40a67d8e8c
JeremyGillard <gillard.jeremy@outlook.fr>
2021-02-16 17:26:25 +0100

    feat(sentinel): display + bufferization

--- sentinel/app/sniffer.py
+++ sentinel/app/sniffer.py
@@ -1,28 +1,30 @@
 from scapy.all import sniff, IP
 from datetime import datetime
+from copy import deepcopy
 
 class Packet:
-  def __init__(self, src, sport, dst, dport, proto):
-    self.id = f'{src}:{sport} - {dst}:{dport}'
+  def __init__(self, src, sport, dst, dport, proto, iteration = 0):
+    self.id = f'{src}:{sport}-{dst}:{dport}'
     self.src = src
     self.sport = sport
     self.dst = dst
     self.dport = dport
     self.proto = proto
+    self.iteration = iteration
 
   def __str__(self):
-    return f'{self.src:15}:{self.sport:<5} - {self.dst:15}:{self.dport:<5}'
+    return f'{self.src:15}:{self.sport:<5} - {self.dst:15}:{self.dport:<5} - {self.iteration:9}'
 
   def header(self):
-    return f'{"src":15}:{"sport":<5} - {"dst":15}:{"dport":<5}'
+    return f'{"src":15}:{"sport":<5} - {"dst":15}:{"dport":<5} - iteration - [Header]'
 
 class Sniffer:
   def __init__(self):
-    self.display_count = 0
     self.packets_buffer = {}
+    self.display_count = 0
     self.reference_time = datetime.now()
 
-  def listen(self, display = False):
+  def listen(self):
     sniff(prn=self.process)
 
   def process(self, scapy_pkt):
@@ -37,12 +39,17 @@ class Sniffer:
     if (self.display_count > 20):
       self.display_count = 0
     if (self.display_count == 0):
-      print(f'{"Datetime":28} {packet.header()}    [header]')
+      print(f'{"Datetime":28} {packet.header()}')
+    packet = self.packets_buffer[packet.id]
     print(f'[{time}] {packet}')
     self.display_count += 1
 
   def bufferize(self, packet):
-    pass
+    if packet.id in self.packets_buffer:
+      self.packets_buffer[packet.id].iteration += 1
+    else:
+      packet.iteration = 1
+      self.packets_buffer[packet.id] = packet
 
   def store(self):
     pass
