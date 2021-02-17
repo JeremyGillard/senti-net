6dc93937ce69e4bbc79905f9dbe29c9ac38acc58
JeremyGillard <gillard.jeremy@outlook.fr>
2021-02-17 21:18:28 +0100

    refactor(backend): deletion

--- backend/README.md
+++
@@ -1,5 +0,0 @@
-# Strapi application
-
-A quick description of your strapi application
-
-P@sswOrd1234
--- backend/extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || 'ee9af87c-a9c3-4eee-abbc-37e7316a77cc'
-};
