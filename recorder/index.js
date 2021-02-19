d590289f8f01c8a2feb79d2668dedd8805a193db
JeremyGillard <gillard.jeremy@outlook.fr>
2021-02-19 12:46:32 +0100

    feat(recorder): database storage AND static and dynmaic data server SEPARATION

--- recorder/index.js
+++ recorder/index.js
@@ -5,7 +5,7 @@ const subscriber = redis.createClient();
 subscriber.on("message", function (channel, message) {
   if (channel === "packets") {
     const packet = JSON.parse(message);
-    console.log("Serve: ", packet);
+    console.log("Record: ", packet);
   }
 });
 
