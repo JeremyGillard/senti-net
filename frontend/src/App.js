d252e21e94f65bc82fcde7ec3c05e2f7a75818d6
JeremyGillard <gillard.jeremy@outlook.fr>
2021-02-24 22:33:35 +0100

    feat(frontend): first d3 graph

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import * as d3 from 'd3';
 
-function App() {
+const data = [
+  {
+    country: 'United States',
+    countryCode: 'US',
+    region: 'VA',
+    regionName: 'Virginia',
+    city: 'Ashburn',
+    zip: '20149',
+    lat: 39.03,
+    lon: -77.5,
+    timezone: 'America/New_York',
+    isp: 'Google LLC',
+    org: 'Google Public DNS',
+    as: 'AS15169 Google LLC',
+    query: '8.8.8.8',
+    iteration: 248,
+  },
+  {
+    country: 'Canada',
+    countryCode: 'CA',
+    region: 'QC',
+    regionName: 'Quebec',
+    city: 'Montreal',
+    zip: 'H1S',
+    lat: 45.5808,
+    lon: -73.5825,
+    timezone: 'America/Toronto',
+    isp: 'Le Groupe Videotron Ltee',
+    org: 'Videotron Ltee',
+    as: 'AS5769 Videotron Telecom Ltee',
+    query: '24.48.0.1',
+    iteration: 154,
+  },
+  {
+    country: 'China',
+    countryCode: 'CN',
+    region: 'FJ',
+    regionName: 'Fujian',
+    city: 'Fuzhou',
+    zip: '',
+    lat: 26.0745,
+    lon: 119.296,
+    timezone: 'Asia/Shanghai',
+    isp: 'Chinanet',
+    org: 'Chinanet FJ',
+    as: 'AS4134 CHINANET-BACKBONE',
+    query: '110.90.1.0',
+    iteration: 177,
+  },
+  {
+    country: 'United States',
+    countryCode: 'US',
+    region: 'AZ',
+    regionName: 'Arizona',
+    city: 'Sierra Vista',
+    zip: '85613',
+    lat: 31.5552,
+    lon: -110.35,
+    timezone: 'America/Phoenix',
+    isp: 'DoD Network Information Center',
+    org: 'USAISC',
+    as: 'AS335 DoD Network Information Center',
+    query: '55.244.122.20',
+    iteration: 302,
+  },
+];
+
+export default function App() {
+  useEffect(() => {
+    d3.select('#graph')
+      .selectAll('rect')
+      .data(data, (d) => d)
+      .join('rect')
+      .attr('x', (d, i) => i * 50)
+      .attr('y', (d) => 400 - d.iteration)
+      .attr('width', 50)
+      .attr('height', (d) => d.iteration)
+      .attr('fill', '#F0FFFF')
+      .attr('stroke', '#5F9EA0')
+      .attr('stroke-width', 1)
+      .append('text')
+      .text((d) => d.country);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <svg id="graph" width="400" height="400"></svg>
     </div>
   );
 }
-
-export default App;
