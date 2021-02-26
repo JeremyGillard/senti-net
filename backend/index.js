b4d36f6e90cd30b4b63e2f88814e55f1ea0affd7
JeremyGillard <gillard.jeremy@outlook.fr>
2021-02-26 15:41:54 +0100

    block axios post array

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,64 @@
-const redis = require("redis");
+const redis = require('redis');
+const axios = require('axios');
+
+const db = require('./db');
 
 const subscriber = redis.createClient();
 
-subscriber.on("message", function (channel, message) {
-  if (channel === "packets") {
+async function insertPacket(packet) {
+  try {
+    const query = {
+      text:
+        'INSERT INTO packets (capture_datetime, src, sport, dst, dport, proto) VALUES ($1, $2, $3, $4, $5, $6)',
+      values: Object.values(packet),
+    };
+
+    await db.query(query);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function getSrcIps() {
+  try {
+    const query = {
+      text: 'SELECT DISTINCT(src) FROM packets WHERE src NOT LIKE $1',
+      values: ['192.168.0.%'],
+    };
+
+    const { rows } = await db.query(query);
+    return rows.map((row) => row.src);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function getGeoIps() {
+  try {
+    const ips = await getSrcIps();
+    console.log(ips);
+    const {
+      data,
+    } = await axios.post(
+      'http://ip-api.com/batch?fields=country,region,city,lat,lon,isp,org,as,query',
+      { ips },
+    );
+    console.log(data);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+subscriber.on('message', function (channel, message) {
+  if (channel === 'packets') {
     const packet = JSON.parse(message);
-    console.log("Serve: ", packet);
+    insertPacket(packet);
+    // console.log(packet);
   }
 });
 
-subscriber.subscribe("packets");
+setInterval(() => {
+  getGeoIps();
+}, 5000);
+
+subscriber.subscribe('packets');
