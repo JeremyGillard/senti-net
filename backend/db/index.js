2dbbfb6310d6cb6564fb78c7518578669efdcc00
JeremyGillard <gillard.jeremy@outlook.fr>
2021-03-02 23:15:25 +0100

    feat(banckend): db manip

--- backend/db/index.js
+++ backend/db/index.js
@@ -8,8 +8,52 @@ const pool = new Pool({
   port: 5432,
 });
 
+async function insertPacket(packet) {
+  try {
+    const query = {
+      text:
+        'INSERT INTO packets (capture_datetime, src, sport, dst, dport, proto) VALUES ($1, $2, $3, $4, $5, $6)',
+      values: Object.values(packet),
+    };
+
+    await pool.query(query);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function getSrcIps() {
+  try {
+    const query = {
+      text: 'SELECT DISTINCT(src) FROM packets WHERE src NOT LIKE $1',
+      values: ['192.168.0.%'],
+    };
+
+    const { rows } = await db.query(query);
+    return rows.map((row) => row.src);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function getGeoIps() {
+  try {
+    const ips = await getSrcIps();
+    console.log(ips);
+    // const {
+    //   data,
+    // } = await axios.post(
+    //   'http://ip-api.com/batch?fields=country,region,city,lat,lon,isp,org,as,query',
+    //   { ips },
+    // );
+    // console.log(data);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
 module.exports = {
-  query: (text, params, callback) => {
-    return pool.query(text, params, callback);
-  },
+  insertPacket: insertPacket,
+  getSrcIps: getSrcIps,
+  getGeoIps: getGeoIps,
 };
--- backend/index.js
+++ backend/index.js
@@ -1,53 +1,8 @@
 const redis = require('redis');
-const axios = require('axios');
 
 const db = require('./db');
 
 const subscriber = redis.createClient();
-
-async function insertPacket(packet) {
-  try {
-    const query = {
-      text:
-        'INSERT INTO packets (capture_datetime, src, sport, dst, dport, proto) VALUES ($1, $2, $3, $4, $5, $6)',
-      values: Object.values(packet),
-    };
-
-    await db.query(query);
-  } catch (error) {
-    console.error(error);
-  }
-}
-
-async function getSrcIps() {
-  try {
-    const query = {
-      text: 'SELECT DISTINCT(src) FROM packets WHERE src NOT LIKE $1',
-      values: ['192.168.0.%'],
-    };
-
-    const { rows } = await db.query(query);
-    return rows.map((row) => row.src);
-  } catch (error) {
-    console.error(error);
-  }
-}
-
-async function getGeoIps() {
-  try {
-    const ips = await getSrcIps();
-    console.log(ips);
-    const {
-      data,
-    } = await axios.post(
-      'http://ip-api.com/batch?fields=country,region,city,lat,lon,isp,org,as,query',
-      { ips },
-    );
-    console.log(data);
-  } catch (error) {
-    console.error(error);
-  }
-}
 
 subscriber.on('message', function (channel, message) {
   if (channel === 'packets') {
@@ -57,8 +12,15 @@ subscriber.on('message', function (channel, message) {
   }
 });
 
-setInterval(() => {
-  getGeoIps();
-}, 5000);
-
 subscriber.subscribe('packets');
+
+async function getData() {
+  const result = await db.getSrcIps();
+  console.log(result);
+}
+
+function exec() {
+  getData();
+}
+
+exec();
